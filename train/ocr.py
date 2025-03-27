ee43d3bfb73710e8b97ead7cea50fe6b0cb12070
PyDevC <anuskcse@gmail.com>
2025-03-27 19:20:35 +0530

    cleaning repo

--- train/ocr.py
+++
@@ -1,8 +0,0 @@
-import pytesseract
-import cv2
-import importlib.util
-import numpy as np
-
-def detect_text(pdf_path):
-    img = cv2.imread(pdf_path)
-    return pytesseract.image_to_string(img)
--- utils/readpdf.py
+++
@@ -1,7 +0,0 @@
-# import module
-from pdf2image import convert_from_path
-from PIL import Image
-
-def extract_images(pdf_path):
-    images = convert_from_path(pdf_path)
-    image = Image(images)
